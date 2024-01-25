1fc95fae7d352214f8d729cf0896706ecacc08a8
Maarten beute <150816290+ToMaarton@users.noreply.github.com>
2024-01-25 17:51:24 +0100

    added function to generate color map

--- sketch.js
+++ sketch.js
@@ -1,27 +1,55 @@
 let heightmap;
 function setup() {
-  createCanvas(600,600);
-  heightmap = generateHeightMap(600,0.02);
+  createCanvas(windowHeight,windowHeight);
+  //noiseSeed(1)
+  heightmap = generateHeightMap(height,0.015);
+  colormap = generateColorMap(heightmap);
 }
 
 function draw() {
   background(0);
-  image(heightmap,0,0);
+  image(colormap,0,0);
   print('img');
   noLoop();
 }
 
 function generateHeightMap(size, roughness){
-  noiseDetail(2,0.5)
+  noiseDetail(10,0.5)
   let img = createImage(width, height);
   img.loadPixels();
   for (let x = 0; x < img.width; x += 1) {
     for (let y = 0; y < img.height; y += 1) {
       let nx = x*roughness;
       let ny = y*roughness;
-      img.set(x, y, noise(nx,ny)*255-dist(width/2,height/2,x,y) *(255/(size/2)));
+      let terrainHeight = (noise(nx,ny)-(dist(width/2,height/2,x,y)/(size/2)))*400;
+      img.set(x, y, terrainHeight);
     }
   }
   img.updatePixels();
+  return img;
+}
+
+function generateColorMap(HM){
+  let img = createImage(width, height);
+  HM.loadPixels();
+  img.loadPixels();
+  for (let x = 0; x < img.width; x += 1) {
+    for (let y = 0; y < img.height; y += 1) {
+      H = HM.get(x,y)[1]
+      let c;
+      if(H>200){
+        c = color(100);
+      }else if(H>100){
+        c = color(100,150,50);
+      }else if(H>50){
+        c = color(200,250,100);
+      }else{
+        c = color(250,250,200)
+      }
+      img.set(x, y, c);
+    }
+  }
+  img.updatePixels();
+  HM.updatePixels();
   return img;
 }
