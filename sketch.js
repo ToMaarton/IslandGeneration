01ab38f382730f6090caceae02e37dfdc56b9aea
Maarten beute <150816290+ToMaarton@users.noreply.github.com>
2024-01-25 16:47:40 +0100

    added function to generate terain height

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
+let heightmap;
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600,600);
+  heightmap = generateHeightMap(600,0.02);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  image(heightmap,0,0);
+  print('img');
+  noLoop();
+}
+
+function generateHeightMap(size, roughness){
+  noiseDetail(2,0.5)
+  let img = createImage(width, height);
+  img.loadPixels();
+  for (let x = 0; x < img.width; x += 1) {
+    for (let y = 0; y < img.height; y += 1) {
+      let nx = x*roughness;
+      let ny = y*roughness;
+      img.set(x, y, noise(nx,ny)*255-dist(width/2,height/2,x,y) *(255/(size/2)));
+    }
+  }
+  img.updatePixels();
+  return img;
 }
